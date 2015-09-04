88dbb27b294c58e00b2a4210fb882ef0e4c02cbc
Aman Bindlish <bindlish.aman@snapdeal.com>
2015-09-04 15:22:13 +0530

    changes

--- app/src/main/java/main/bindroid/sdattendance/LoginActivity.java
+++ app/src/main/java/main/bindroid/sdattendance/LoginActivity.java
@@ -1,17 +1,18 @@
 package main.bindroid.sdattendance;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
 	private ImageView snapdealImageView;
 	private LinearLayout mainLinearLayout;
+	private TextView getInText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +20,24 @@ public class LoginActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_login);
 		snapdealImageView = (ImageView) findViewById(R.id.sdlogo);
 		mainLinearLayout = (LinearLayout) findViewById(R.id.mainLinear);
-		findViewById(R.id.getInTv).setOnClickListener(new OnClickListener() {
+		getInText=(TextView)findViewById(R.id.getInTv);
+		getInText.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				startActivity(new Intent(LoginActivity.this,
-						DashboardActivity.class));
+				validateDataFromDB();
+//				startActivity(new Intent(LoginActivity.this,
+//						DashboardActivity.class));
 			}
 		});
 		snapdealImageView.animate().translationYBy(-500).setDuration(2000).setStartDelay(2000)
 				.start();
 		mainLinearLayout.animate().alpha(1).setDuration(4000).setStartDelay(2000).start();
 	}
+
+	public void validateDataFromDB(){
+
+	}
+
 
 }
