4413be094ae953adae284b8f0812963e9b7c3f59
Dev.Dhar <Dev.dhar@snapdeal.com>
2015-09-04 22:48:51 +0530

    Location pin point added.

---
+++ app/src/main/java/main/bindroid/sdattendance/Location.java
@@ -0,0 +1,32 @@
+package main.bindroid.sdattendance;
+
+/**
+ * Created by Dev.Dhar on 04/09/15.
+ */
+public class Location {
+
+	int x;
+	int y;
+
+	public Location(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+}
