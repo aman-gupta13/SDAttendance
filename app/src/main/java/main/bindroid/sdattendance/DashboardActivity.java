43a1435ee70000037d016ddeb32990f9a4728f2c
Aman Bindlish <bindlish.aman@snapdeal.com>
2015-09-04 18:33:37 +0530

    beacon logic added

--- app/src/main/java/main/bindroid/sdattendance/DashboardActivity.java
+++ app/src/main/java/main/bindroid/sdattendance/DashboardActivity.java
@@ -1,6 +1,11 @@
 package main.bindroid.sdattendance;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,18 +13,58 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class DashboardActivity extends AppCompatActivity {
 
+	private static final int REQUEST_ENABLE_BT = 1234;
 	private ViewPager viewPager;
 	private TabLayout tabLayout;
+	private BeaconManager mBeaconManager;
+	private Region mRegion;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dahsboard);
+
+		// doing beacon work here
+		mRegion = new Region(Globals.REGION + "_", Globals.PROXIMITY_UUID,
+				Globals.MAJOR, Globals.MINOR);
+		// Configure mBeaconManager.
+		mBeaconManager = new BeaconManager(this);
+		// Default values are 5s of scanning and 25s of waiting time to save CPU
+		// cycles.
+		// In order for this demo to be more responsive and immediate we lower
+		// down those values.
+		mBeaconManager.setBackgroundScanPeriod(TimeUnit.SECONDS.toMillis(1), 0);
+		mBeaconManager
+				.setMonitoringListener(new BeaconManager.MonitoringListener() {
+
+					@Override
+					public void onEnteredRegion(final Region region,
+							List<Beacon> beacons) {
+						// Todo: do something when region entered
+					}
+
+					@Override
+					public void onExitedRegion(final Region region) {
+						// Todo: do something when region exited
+					}
+				});
+		// starting beacon service here if bluetooth is on and never started
+		if (!getSharedPreferences("SDAttendance", Context.MODE_PRIVATE)
+				.getBoolean("service", false))
+			startBeaconService();
+
 		tabLayout = (TabLayout) findViewById(R.id.tabLayout);
 		viewPager = (ViewPager) findViewById(R.id.viewpager);
 		tabLayout.addTab(tabLayout.newTab().setText("Attendence"));
@@ -27,6 +72,89 @@ public class DashboardActivity extends AppCompatActivity {
 		viewPager.setOnPageChangeListener(onPageChangeListener);
 		viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
 		tabLayout.setupWithViewPager(viewPager);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Check if device supports Bluetooth Low Energy.
+		if (!mBeaconManager.hasBluetooth()) {
+			Toast.makeText(this, getString(R.string.device_no_ble),
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (mBeaconManager.hasBluetooth())
+			mBeaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+
+				@Override
+				public void onServiceReady() {
+					try {
+						mBeaconManager.startMonitoring(mRegion);
+					} catch (RemoteException e) {
+						Log.d("DashboardActivity",
+								"Error while starting monitoring");
+					}
+				}
+			});
+	}
+
+	@Override
+	protected void onDestroy() {
+		mBeaconManager.disconnect();
+		super.onDestroy();
+	}
+
+	@Override
+	protected void
+			onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT) {
+			if (resultCode == Activity.RESULT_OK) {
+				startBeaconService();
+			} else {
+				Toast.makeText(this, getString(R.string.bluetooth_not_enabled),
+						Toast.LENGTH_LONG).show();
+			}
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+
+	private boolean checkBluetooth() {
+		// Check if device supports Bluetooth Low Energy.
+		if (!mBeaconManager.hasBluetooth()) {
+			Toast.makeText(this, getString(R.string.device_no_ble),
+					Toast.LENGTH_LONG).show();
+			return false;
+		}
+		// If Bluetooth is not enabled, let user enable it.
+		if (!mBeaconManager.isBluetoothEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		} else {
+			return true;
+		}
+		return false;
+	}
+
+	private void startBeaconService() {
+		if (checkBluetooth()) {
+			startService(new Intent(DashboardActivity.this, MyService.class));
+			getSharedPreferences("SDAttendance", Context.MODE_PRIVATE).edit()
+					.putBoolean("service", true).commit();
+			Toast.makeText(DashboardActivity.this,
+					getString(R.string.service_started), Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+	private void stopBeaconService() {
+		stopService(new Intent(DashboardActivity.this, MyService.class));
+		Toast.makeText(DashboardActivity.this,
+				getString(R.string.service_stopped), Toast.LENGTH_SHORT).show();
 	}
 
 	OnPageChangeListener onPageChangeListener = new OnPageChangeListener() {
@@ -47,28 +175,6 @@ public class DashboardActivity extends AppCompatActivity {
 
 		}
 	};
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_dahsboard, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		// noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
 
 	public class MyPagerAdapter extends FragmentPagerAdapter {
 
